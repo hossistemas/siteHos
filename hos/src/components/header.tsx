4095d507274f1d255acc1e66248e2335dfda109a
Guilherme da Rocha <guis.darocha@gmail.com>
2023-07-22 16:38:14 -0300

    adicionado animação menu responsivo

--- hos/src/components/header.tsx
+++ hos/src/components/header.tsx
@@ -3,6 +3,7 @@
 
 import logo from '../../public/logo.svg'
 import hamburguer from '../../public/hamburguer.svg'
+import xis from '../../public/xis.svg'
 import Image from 'next/image'
 import { useState } from 'react'
 
@@ -13,7 +14,6 @@ interface HeaderProps {
 export function Header(props: HeaderProps){
     const [botao1Visivel, setbotao1Visivel] = useState(true)
     const [botao2Visivel, setbotao2Visivel] = useState(false)
-    const [iniciar, setIniciar] = useState(false)
 
     const visibilidadeBotao = () => {
         setbotao1Visivel((prev) => !prev)
@@ -34,8 +34,8 @@ export function Header(props: HeaderProps){
             )}
             {botao2Visivel && (
                 <button onClick={visibilidadeBotao} 
-                className='absolute px-[0.8rem] font-semibold text-vermelho text-[rem] right-11 w-[3rem] h-auto flex border-2 border-vermelho rounded-md   bg-white cursor-pointer tablet:hidden '>
-                    X
+                className='absolute  py-[1rem] px-[1rem] right-11 w-[5rem] h-auto flex border-2 border-vermelho rounded-md   bg-white cursor-pointer tablet:hidden '>
+                    <Image src={xis} alt='menu hamburguer' className='w-[4rem] '/>
                 </button>
             )}
 
@@ -63,20 +63,20 @@ export function Header(props: HeaderProps){
             
         <div className={`bg-vermelho ${botao1Visivel ? 'hidden' : 'visible transition-all duration-300'}`}>
 
-            <div className='tablet:flex tablet:justify-center tablet:items-center tablet:mx-auto tablet:text-[0.8rem] tablet:font-semibold tablet:text-grafite '>               
-                    <a href="" className='mr-[1.8rem] tablet:ml-[1rem]'>Soluções</a>
-                    <a href="" className='mr-[1.8rem] '>Sobre</a>
-                    <a href="" className='mr-[1.8rem] '>Suporte</a>
-                    <a href="" className='mr-[1.8rem] '>Universidade</a>
-                    <a href="" className='mr-[3rem]' >Blog</a>
+            <div className='grid gap-y-3 text-white pt-[1rem] ml-[3rem] text-[1rem] font-semibold tablet:flex tablet:justify-center tablet:items-center tablet:mx-auto tablet:text-[0.8rem] tablet:font-semibold tablet:text-grafite '>               
+                    <a href="" className=''>Soluções</a>
+                    <a href="" className=' '>Sobre</a>
+                    <a href="" className=''>Suporte</a>
+                    <a href="" className=''>Universidade</a>
+                    <a href="" className='' >Blog</a>
             </div>
 
-            <div className='tablet:flex items-center'>
-                <div className='tablet:flex tablet:justify-center tablet:items-center'>
-                    <button className='tablet:bg-gradient-to-b tablet:from-laranja tablet:to-magenta tablet:hover:bg-gradient-to-b tablet:hover:from-darker tablet:hover:to-darker tablet:hover:duration-1000 tablet:text-[0.8rem] tablet:font-semibold tablet:text-white tablet:rounded-md tablet:w-[13rem] tablet:px-[1.5rem] tablet:py-[1rem] '>Solicite uma demonstração</button>
+            <div className=''>
+                <div className='py-[1rem]'>
+                    <button className='bg-darker ml-[3rem] mt-[0.1rem] text-[1rem] font-semibold text-white rounded-md py-[0.5rem] px-[0.8rem]'>Solicite uma amostra</button>
                 </div>
-                <div className='tablet:flex tablet:ml-[2rem] tablet:justify-center tablet:items-center tablet:bg-white tablet:border-2 tablet:border-vermelho tablet:rounded-md tablet:w-[13rem] tablet:px-[1.5rem] tablet:py-[0.8rem]'>
-                    <button className='tablet:text-[0.8rem] tablet:font-semibold  tablet:text-vermelho'>Área do cliente</button>
+                <div className='ml-[3rem] py-[0.2rem] pb-[2rem]'>
+                    <button className='bg-vermelho text-[1rem] font-semibold text-white rounded-md border-2 border-white py-[0.5rem] px-[0.8rem]'>Área do cliente</button>
                 </div>
             </div>
         </div>
