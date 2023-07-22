afedd0ccc4cb64a4aa88643a7d2962d9b8a24dce
Guilherme da Rocha <guis.darocha@gmail.com>
2023-07-22 13:41:25 -0300

    componente header iniciado

--- hos/src/components/header.tsx
+++ hos/src/components/header.tsx
@@ -1,4 +1,4 @@
-import logo from '../../public/logo.png'
+import logo from '../../public/logo.svg'
 import Image from 'next/image'
 
 interface HeaderProps {
@@ -7,22 +7,22 @@ interface HeaderProps {
 
 export function Header(props: HeaderProps){
     return(
-        <header className='flex px-[8rem]'>
-            <Image src={logo} alt={'logo-hos'} />
-            <div>
-                <a href="">Soluções</a>
-                <a href="">Sobre</a>
-                <a href="">Suporte</a>
-                <a href="">Universidade</a>
-                <a href="">Blog</a>
+        <header className='flex items-center mx-auto px-[8rem] py-[1.8rem] '>
+            <Image src={logo} alt={'logo-hos'} className='w-[4.8rem] h-auto' />
+            <div className='text-[0.8rem] font-semibold  text-grafite '>
+                <a href="" className='px-[1.8rem]'>Soluções</a>
+                <a href="" className='px-[1.8rem]'>Sobre</a>
+                <a href="" className='px-[1.8rem]'>Suporte</a>
+                <a href="" className='px-[1.8rem]'>Universidade</a>
+                <a href="" className='px-[1.8rem]'>Blog</a>
             </div>
 
-            <div>
-                <div>
-                    <button></button>
+            <div className=' flex items-center ml-[8rem]'>
+                <div className='flex justify-center items-center bg-gradient-to-b from-laranja to-magenta rounded-md w-[13rem] px-[1.5rem] py-[1rem]'>
+                    <button className='text-[0.8rem] font-semibold text-white'>Solicite uma demonstração</button>
                 </div>
-                <div>
-                    <button></button>
+                <div className='flex ml-[2rem] justify-center items-center bg-white border-2 border-vermelho rounded-md w-[13rem] px-[1.5rem] py-[0.8rem]'>
+                    <button className='text-[0.8rem] font-semibold  text-vermelho'>Área do cliente</button>
                 </div>
             </div>
         </header>
--- hos/tailwind.config.js
+++ hos/tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors:{
+        'grafite': '#212121',
+        'laranja': '#FF6251',
+        'magenta': '#FF4358',
+        'darker': '#4C1813',
+        'vermelho': '#DA4536',
+      }
     },
   },
   plugins: [],
