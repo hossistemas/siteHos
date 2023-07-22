57b2d3b1dc57b47adecd017069a96f398169525d
Guilherme da Rocha <guis.darocha@gmail.com>
2023-07-22 13:52:40 -0300

    componente header iniciado

--- hos/src/components/header.tsx
+++ hos/src/components/header.tsx
@@ -8,18 +8,18 @@ interface HeaderProps {
 export function Header(props: HeaderProps){
     return(
         <header className='flex items-center mx-auto px-[8rem] py-[1.8rem] '>
-            <Image src={logo} alt={'logo-hos'} className='w-[4.8rem] h-auto' />
-            <div className='text-[0.8rem] font-semibold  text-grafite '>
-                <a href="" className='px-[1.8rem]'>Soluções</a>
-                <a href="" className='px-[1.8rem]'>Sobre</a>
-                <a href="" className='px-[1.8rem]'>Suporte</a>
-                <a href="" className='px-[1.8rem]'>Universidade</a>
-                <a href="" className='px-[1.8rem]'>Blog</a>
+            <Image src={logo} alt={'logo-hos'} className='w-auto h-[3rem]' />
+            <div className='flex justify-center items-center mx-autotext-[0.8rem] font-semibold  text-grafite '>
+                <a href="" className='mr-[1.8rem] ml-[6rem]'>Soluções</a>
+                <a href="" className='mr-[1.8rem]'>Sobre</a>
+                <a href="" className='mr-[1.8rem]'>Suporte</a>
+                <a href="" className='mr-[1.8rem]'>Universidade</a>
+                <a href="" className='mr-[14rem]'>Blog</a>
             </div>
 
-            <div className=' flex items-center ml-[8rem]'>
-                <div className='flex justify-center items-center bg-gradient-to-b from-laranja to-magenta rounded-md w-[13rem] px-[1.5rem] py-[1rem]'>
-                    <button className='text-[0.8rem] font-semibold text-white'>Solicite uma demonstração</button>
+            <div className=' flex items-center'>
+                <div className='flex justify-center items-center'>
+                    <button className='bg-gradient-to-b from-laranja to-magenta hover:bg-gradient-to-b hover:from-darker hover:to-darker text-[0.8rem] font-semibold text-white rounded-md w-[13rem] px-[1.5rem] py-[1rem] '>Solicite uma demonstração</button>
                 </div>
                 <div className='flex ml-[2rem] justify-center items-center bg-white border-2 border-vermelho rounded-md w-[13rem] px-[1.5rem] py-[0.8rem]'>
                     <button className='text-[0.8rem] font-semibold  text-vermelho'>Área do cliente</button>
