35e0c225e215d6cc76b4a263a543085c7a245a47
Guilherme da Rocha <guis.darocha@gmail.com>
2023-07-23 10:17:17 -0300

    Finalizado objetos dinamicos

--- hos/src/app/page.tsx
+++ hos/src/app/page.tsx
@@ -1,16 +1,19 @@
 import { Header } from "@/components/header";
 import { HeroSection } from "@/components/herosection/herosection";
-
+import banner from '../../public/banner1.png'
 
 export default function Home() {
   return (
     <>
     <Header />
     <HeroSection 
-      bgClassName={"bg"}
+      bgClassName={"bg-gradient-to-b from-laranja to-magenta"}
       title={"Sistema intuitivo, processos claros e suporte simplificado!"} 
+      titleClassName={"text-white"}
       text={"Temos a receita certa para o sucesso da sua drogaria ou farmácia de manipulação."} 
-      img={undefined}
+      textClassName={"text-white"}
+      buttonClassName={"bg-darker text-white"}
+      img={banner}
     />
 
       
--- hos/src/components/herosection/herosection.tsx
+++ hos/src/components/herosection/herosection.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import Image from 'next/image'
 import { useState } from 'react'
 
@@ -8,21 +6,31 @@ interface HeroSectionProps {
     text: string;
     img: any;
     bgClassName: any,
+    titleClassName: any,
+    textClassName: any,
+    buttonClassName: any,
 }
 
-export function HeroSection({title, text, img, bgClassName}: HeroSectionProps){
+export function HeroSection({title, text, img, bgClassName, titleClassName, textClassName, buttonClassName}: HeroSectionProps){
     
     return(
         <>
         
-        <div className={`${bgClassName}`}>
-            <h1>{title}</h1>
-            <p>{text}</p>
+        <div className={`drop-shadow-lg mb-[5rem] ${bgClassName}`}>
+            
+            <div className='pt-[3rem] px-[3.5rem]'>
+                <h1 className={`text-[1.5rem] font-bold leading-none ${titleClassName}`}>{title}</h1>
+                <p className={`text-[0.8rem] font-medium my-[1rem] ${textClassName}`}>{text}</p>
+                <button className={`text-[1rem] font-regular mb-[1.3rem] px-[1.3rem] py-[0.4rem] rounded-md ${buttonClassName}`}>Solicite uma demonstração</button>
+            </div>
+
+            <div className='flex justify-center'>
+                <Image src={img} alt='' className='h-auto w-[15rem]'/>
+            </div>
+
+
         </div>
-        <div>
-            <Image src={img} alt=''/>
-        </div>
-     
+
         </>
         )    
 
