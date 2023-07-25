0b9bb25fea1bea1efa1f8bb1251b99f5dcea7c11
Guilherme da Rocha <guis.darocha@gmail.com>
2023-07-24 21:25:07 -0300

    Tentativa falha de fazer carrossel

--- hos/src/components/cardSistema.tsx
+++ hos/src/components/cardSistema.tsx
@@ -10,13 +10,19 @@ interface cardProps {
 }
 
 
-
-export function CardSistema({img , titulo , texto}: cardProps){
+export function CardSistema({ img, titulo , texto}: cardProps){
   
     return( 
     
         
-        <div></div>
+        <div className='bg-white  flex justify-center items-center mx-auto drop-shadow-lg w-[16.375rem] h-[15.437rem] rounded-md  '>
+            <div >
+            <Image src={ img } alt='' className='w-[3.8rem] h-auto flex justify-center items-center mx-auto mt-[2rem] mb-[1.8rem]'/>
+
+                  <h3 className='text-[1rem] text-grafite font-semibold text-center mb-[0.9rem]'>{titulo}</h3>
+                  <p className='w-[13.93rem] mx-auto text-[0.8rem] text-grafite font-normal text-center mb-[2rem]'>{texto}</p>
+              </div>
+        </div>
         
 
     
--- hos/src/components/sistema.tsx
+++ hos/src/components/sistema.tsx
@@ -13,12 +13,6 @@ interface SistemaProps {
 }
 
 
-
-
-
-
-
-
 export function Sistema(props: SistemaProps){
     const [currentIndex, setCurrentIndex] = useState(0)
 
@@ -54,8 +48,34 @@ export function Sistema(props: SistemaProps){
                 <div className='group-hover:block'>
                     <Image src={arrow} alt='' className='hidden rotate-180' onClick={prevCard}/>
                 </div>
+                
+                <div  className='bg-white  flex justify-center items-center mx-auto drop-shadow-lg w-[16.375rem] h-[15.437rem] rounded-md'  >
+                    <div >
+                        <Image src={ lucro } alt='' className='w-[3.8rem] h-auto flex justify-center items-center mx-auto mt-[2rem] mb-[1.8rem]'/>
+                        <h3 className='text-[1rem] text-grafite font-semibold text-center mb-[0.9rem]'>Maximize sua margem de lucro</h3>
+                        <p className='w-[13.93rem] mx-auto text-[0.8rem] text-grafite font-normal text-center mb-[2rem]'>Venda com inteligência e rentabilidade. Encontre alternativas mais rentáveis ao consultar um produto e aumente sua margem de lucro.</p>
+                    </div>
+                 </div>
 
-                <Image src={cards[currentIndex]} alt=''/>
+                 <div  className='bg-white  flex justify-center items-center mx-auto drop-shadow-lg w-[16.375rem] h-[15.437rem] rounded-md' >
+                    <div >
+                        <Image src={ lucro } alt='' className='w-[3.8rem] h-auto flex justify-center items-center mx-auto mt-[2rem] mb-[1.8rem]'/>
+                        <h3 className='text-[1rem] text-grafite font-semibold text-center mb-[0.9rem]'>Maximize sua margem de lucro</h3>
+                        <p className='w-[13.93rem] mx-auto text-[0.8rem] text-grafite font-normal text-center mb-[2rem]'>Venda com inteligência e rentabilidade. Encontre alternativas mais rentáveis ao consultar um produto e aumente sua margem de lucro.</p>
+                    </div>
+                 </div>
+
+                 <div  className='bg-white  flex justify-center items-center mx-auto drop-shadow-lg w-[16.375rem] h-[15.437rem] rounded-md' >
+                    <div >
+                        <Image src={ lucro } alt='' className='w-[3.8rem] h-auto flex justify-center items-center mx-auto mt-[2rem] mb-[1.8rem]'/>
+                        <h3 className='text-[1rem] text-grafite font-semibold text-center mb-[0.9rem]'>Maximize sua margem de lucro</h3>
+                        <p className='w-[13.93rem] mx-auto text-[0.8rem] text-grafite font-normal text-center mb-[2rem]'>Venda com inteligência e rentabilidade. Encontre alternativas mais rentáveis ao consultar um produto e aumente sua margem de lucro.</p>
+                    </div>
+                 </div>
+                 
+
+
+
             
                 </div>
             <div className='group-hover:block'>
