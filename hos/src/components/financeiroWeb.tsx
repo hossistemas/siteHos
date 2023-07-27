dcb0a7997aa4276dd68dbd3737532c19d70fe3d5
Guilherme da Rocha <guis.darocha@gmail.com>
2023-07-27 19:51:23 -0300

    Finalizada  responsividade componente financeiroWeb

--- hos/src/components/financeiroWeb.tsx
+++ hos/src/components/financeiroWeb.tsx
@@ -10,31 +10,31 @@ export function FinanceiroWeb(props: FinanceiroWebProps){
     
     return(
        <div className="bg-fundo">
-            <div className="py-[3.75rem] tablet:grid grid-cols-3">
-                <h2 className="text-[1.5rem] w-[19rem] mx-auto text-grafite font-extrabold leading-tight text-center tablet:col-span-3">Amplie suas possibilidades com o <span className="text-vermelho">Financeiro Web</span></h2>
+            <div className="py-[3.75rem] tablet:grid grid-cols-3 tablet:mx-auto tablet:max-w-[59.8rem] tablet:py-[6rem]">
+                <h2 className="text-[1.5rem] w-[19rem] mx-auto text-grafite font-extrabold leading-tight text-center tablet:col-span-3 tablet:text-[2rem] tablet:w-[46.8rem] tablet:pb-[5rem]">Amplie suas possibilidades com o <span className="text-vermelho">Financeiro Web</span></h2>
                 
                 <div className="">
-                    <div className="bg-white w-[5.7rem] h-[5.6rem] mt-[1.5rem]  rounded-3xl drop-shadow-2xl flex justify-center mx-auto">
-                        <Image src={gestao} alt="" className=" w-[3.9rem] h-auto"/>
+                    <div className="bg-white w-[5.7rem] h-[5.6rem] mt-[1.5rem]  rounded-3xl drop-shadow-2xl flex justify-center mx-auto tablet:w-[8.1rem] tablet:h-[8rem]">
+                        <Image src={gestao} alt="" className=" w-[3.9rem] h-auto tablet:w-[5rem] tablet:h-auto"/>
                     </div>
-                    <h4 className="w-[13rem] text-[1rem] mt-[1.5rem] font-extrabold text-center text-grafite mx-auto">Gestão da saúde financeira</h4>
-                    <p className="w-[12.25rem] text-[0.87rem] text-center mt-[0.6rem] mx-auto text-grafite">Relatórios DRE Gerencial, plano de contas personalizado, fechamento de convênios.</p>
+                    <h4 className="w-[13rem] text-[1rem] mt-[1.5rem] font-extrabold text-center text-grafite mx-auto tablet:w-[16.1rem] tablet:text-[1.25rem]">Gestão da saúde financeira</h4>
+                    <p className="w-[12.25rem] text-[0.87rem] text-center mt-[0.6rem] mx-auto text-grafite tablet:w-[15.75rem] tablet:text-[1rem]">Relatórios DRE Gerencial, plano de contas personalizado, fechamento de convênios.</p>
                 </div>
 
                 <div className="">
-                    <div className="bg-white w-[5.7rem] h-[5.6rem] mt-[1.5rem] rounded-3xl drop-shadow-2xl flex justify-center mx-auto">
-                        <Image src={boletos} alt="" className=" w-[2.9rem] h-auto"/>
+                    <div className="bg-white w-[5.7rem] h-[5.6rem] mt-[1.5rem] rounded-3xl drop-shadow-2xl flex justify-center mx-auto tablet:w-[8.1rem] tablet:h-[8rem]">
+                        <Image src={boletos} alt="" className=" w-[2.9rem] h-auto tablet:w-[4.4rem] tablet:h-auto"/>
                     </div>
-                    <h4 className="w-[13rem] text-[1rem] mt-[1.5rem] font-extrabold text-center text-grafite mx-auto">Emissão de Boletos</h4>
-                    <p className="w-[12.25rem] text-[0.87rem] text-center mt-[0.6rem] mx-auto text-grafite">Integração com instituições financeiras para facilitar a emissão de remessas e retorno.</p>
+                    <h4 className="w-[13rem] text-[1rem] mt-[1.5rem] font-extrabold text-center text-grafite mx-auto tablet:w-[16.1rem] tablet:text-[1.25rem]">Emissão de Boletos</h4>
+                    <p className="w-[12.25rem] text-[0.87rem] text-center mt-[0.6rem] mx-auto text-grafite tablet:w-[15.75rem] tablet:text-[1rem]">Integração com instituições financeiras para facilitar a emissão de remessas e retorno.</p>
                 </div>
 
                 <div className="">
-                    <div className="bg-white w-[5.7rem] h-[5.6rem] mt-[1.5rem] rounded-3xl drop-shadow-2xl flex justify-center mx-auto">
-                        <Image src={saude} alt="" className=" w-[3.9rem] h-auto"/>
+                    <div className="bg-white w-[5.7rem] h-[5.6rem] mt-[1.5rem] rounded-3xl drop-shadow-2xl flex justify-center mx-auto tablet:w-[8.1rem] tablet:h-[8rem]">
+                        <Image src={saude} alt="" className=" w-[3.9rem] h-auto tablet:w-[5rem] tablet:h-auto"/>
                     </div>
-                    <h4 className="w-[13rem] text-[1rem] mt-[1.5rem] font-extrabold text-center text-grafite mx-auto">Sistema de cobrança</h4>
-                    <p className="w-[12.25rem] text-[0.87rem] text-center mt-[0.6rem] mx-auto text-grafite">Notificações de cobranças via WhatsApp e SMS conforme regra geral especifica.</p>
+                    <h4 className="w-[13rem] text-[1rem] mt-[1.5rem] font-extrabold text-center text-grafite mx-auto tablet:w-[16.1rem] tablet:text-[1.25rem]">Sistema de cobrança</h4>
+                    <p className="w-[12.25rem] text-[0.87rem] text-center mt-[0.6rem] mx-auto text-grafite tablet:w-[15.75rem] tablet:text-[1rem]">Notificações de cobranças via WhatsApp e SMS conforme regra geral especifica.</p>
                 </div>
             </div>
        </div>
--- hos/src/components/solucoes.tsx
+++ hos/src/components/solucoes.tsx
@@ -9,7 +9,7 @@ export function Solucoes(props: SolucoesProps){
     
     return(
         <div className="bg-fundo ">
-            <div className="pt-[2rem] tablet:flex tablet:max-w-[1200px~] ">
+            <div className="pt-[2rem] tablet:flex tablet:max-w-[1200px~] tablet:pt-[4rem]">
                <div className="mx-auto">
                     <h2 className="text-[1.5rem] text-grafite font-extrabold leading-tight text-center tablet:text-left tablet:text-[2rem] tablet:ml-[8rem] tablet:max-w-[31rem]">Soluções para a gestão financeira da sua farmácia</h2>  
                     <p className="text-[0.87rem] text-grafite mx-auto my-[1.38rem] w-[17.2rem] text-center tablet:text-left tablet:text-[1.25rem] tablet:ml-[8rem] tablet:w-[30rem]">Simplifique as operações financeiras da sua farmácia ou drogaria e obtenha uma visão clara dos principais indicadores através 
