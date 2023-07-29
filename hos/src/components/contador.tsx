65a0add0b17b88a56ac015e21a465d7280cd3309
Guilherme da Rocha <guis.darocha@gmail.com>
2023-07-29 15:29:19 -0300

    Iniciado componente contador

--- hos/src/components/contador.tsx
+++ hos/src/components/contador.tsx
@@ -35,14 +35,36 @@ export  function Contador(props: ContadorProps)  {
     
       return (
         <>
-            <div className="bg-fundo pt-5 px-[1rem] grid-cols-2 ">
-                <div className="w-[9.8rem] h-[12.3rem] rounded-lg bg-gradient-to-b from-degradeUm to-degradeDois">
-                <Image src={pdv} alt=''/>
-                <div className="text-white text-[1.25rem] font-extrabold flex">
-                <p>+</p>
-                <Card initialValue={0} finalValue={10}></Card>
-                <p>MIL</p>
+            <div className="bg-fundo pt-5 px-[1rem] flex ">               
+                <div className="w-[9.8rem] h-[12.3rem] mr-[0.8rem] b-[1.5rem]  rounded-lg bg-gradient-to-b from-degradeUm  to-degradeDois">               
+                        <Image src={pdv} alt='' className='mx-auto pt-[1.9rem]'/>
+                        <div className="text-white text-[1.25rem] font-extrabold flex justify-center mt-[1.5rem]">
+                             <p>+</p>
+                             <Card initialValue={0} finalValue={10}></Card>
+                             <p>MIL</p>
+                        </div>
+                        <p className='text-[0.8rem] text-white flex mx-auto justify-center mt-[0.5rem] pb-[1.5rem]'>PDV</p>
                 </div>
+                
+
+                <div className=" w-[9.8rem] h-[12.3rem]  rounded-lg bg-gradient-to-b from-degradeUm to-degradeDois">               
+                        <Image src={brasil} alt='' className='mx-auto pt-[1.9rem]'/>
+                        <div className="text-white text-[1.25rem] font-extrabold flex justify-center mt-[1.5rem]">
+                             <Card initialValue={0} finalValue={26}></Card>
+                        </div>
+                        <p className='text-[0.8rem] text-white flex mx-auto justify-center mt-[0.5rem]'>PDV</p>
+                </div>
+            </div>
+            
+            <div className="bg-fundo pt-[0.8rem] px-[1rem] flex justify-center ">
+                <div className="w-[9.8rem] h-[12.3rem] rounded-lg bg-gradient-to-b from-degradeUm to-degradeDois">               
+                            <Image src={rede} alt='' className='mx-auto pt-[1.9rem]'/>
+                            <div className="text-white text-[1.25rem] font-extrabold flex justify-center mt-[1.5rem]">
+                                <p>+</p>
+                                <Card initialValue={0} finalValue={10}></Card>
+                                <p>MIL</p>
+                            </div>
+                            <p className='text-[0.8rem] text-white flex mx-auto justify-center mt-[0.5rem]'>PDV</p>
                 </div>
             </div>
         </>
