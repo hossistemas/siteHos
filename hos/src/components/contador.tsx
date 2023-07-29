747896dadde547846a316a488562932532f197b4
Guilherme da Rocha <guis.darocha@gmail.com>
2023-07-29 16:11:12 -0300

    Finalizado responsividade componente contador

--- hos/src/components/contador.tsx
+++ hos/src/components/contador.tsx
@@ -28,7 +28,7 @@ export  function Contador(props: ContadorProps)  {
         }, [finalValue]);
     
         return (
-            <p className="text-fundo text-[1.25rem] font-extrabold">{count}</p>
+            <p className="text-fundo text-[1.25rem] font-extrabold tablet:text-[3.12rem]">{count}</p>
 
         );
       };
@@ -36,33 +36,34 @@ export  function Contador(props: ContadorProps)  {
       return (
         <>
             <div className="bg-fundo pt-5 px-[1rem]  ">  
-                <div className='grid grid-cols-4 tablet:w-[1200px] tablet:flex tablet:mx-auto tablet:items-center tablet:justify-center'>
-                    <div className="col-span-2 w-[9.8rem] h-[12.3rem] mr-[0.8rem] b-[1.5rem] rounded-lg bg-gradient-to-b from-degradeUm  to-degradeDois">               
-                            <Image src={pdv} alt='' className='mx-auto pt-[1.9rem]'/>
-                            <div className="text-fundo text-[1.25rem] font-extrabold flex justify-center mt-[1.5rem]">
+                <div className='grid grid-cols-4 tablet:w-[1200px] tablet:flex tablet:mx-auto tablet:items-center tablet:justify-center tablet:mt-[5rem]'>
+                    
+                    <div className="col-span-2 w-[9.8rem] h-[12.3rem] mr-[0.8rem] b-[1.5rem] rounded-lg bg-gradient-to-b from-degradeUm  to-degradeDois tablet:w-[20.8rem] tablet:h-[22.5rem] tablet:rounded-xl tablet:mr-[5rem]">               
+                            <Image src={pdv} alt='' className='mx-auto pt-[1.9rem] tablet:w-[7.8rem] tablet:h-auto tablet:mt-[2rem]'/>
+                            <div className="text-fundo text-[1.25rem] font-extrabold flex justify-center mt-[1.5rem] tablet:text-[3.12rem]">
                                 <p>+</p>
                                 <Card initialValue={0} finalValue={10}></Card>
                                 <p>MIL</p>
                             </div>
-                            <p className='text-[0.8rem] text-fundo flex mx-auto justify-center mt-[0.5rem] pb-[1.5rem]'>PDV</p>
+                            <p className='text-[0.8rem] text-fundo flex mx-auto justify-center mt-[0.5rem] pb-[1.5rem] tablet:text-[1.5rem]'>PDV</p>
                     </div>
                     
 
-                    <div className="col-span-2 w-[9.8rem] h-[12.3rem]  rounded-lg bg-gradient-to-b from-degradeUm to-degradeDois tablet:mr-[0.8rem]">               
-                            <Image src={brasil} alt='' className='mx-auto pt-[1.9rem]'/>
-                            <div className="text-fundo text-[1.25rem] font-extrabold flex justify-center mt-[1.5rem]">
+                    <div className="col-span-2 w-[9.8rem] h-[12.3rem]  rounded-lg bg-gradient-to-b from-degradeUm to-degradeDois tablet:w-[20.8rem] tablet:h-[22.5rem] tablet:rounded-xl tablet:mr-[5rem]">               
+                            <Image src={brasil} alt='' className='mx-auto pt-[1.9rem] tablet:w-[7.8rem] tablet:h-auto tablet:mt-[2rem]'/>
+                            <div className="text-fundo text-[1.25rem] font-extrabold flex justify-center mt-[1.5rem] tablet:text-[3.12rem]">
                                 <Card initialValue={0} finalValue={26}></Card>
                             </div>
-                            <p className='text-[0.8rem] text-fundo flex mx-auto justify-center mt-[0.5rem]'>ESTADOS DO BRASIL</p>
+                            <p className='text-[0.8rem] text-fundo flex mx-auto justify-center mt-[0.5rem] tablet:text-[1.5rem]'>ESTADOS DO BRASIL</p>
                     </div>
 
-                    <div className="col-start-2 col-span-2 w-[9.8rem] h-[12.3rem] mt-[0.5rem] rounded-lg bg-gradient-to-b from-degradeUm to-degradeDois">               
-                                <Image src={rede} alt='' className='mx-auto pt-[1.9rem]'/>
-                                <div className="text-fundo text-[1.25rem] font-extrabold flex justify-center mt-[1.5rem]">
+                    <div className="col-start-2 col-span-2 w-[9.8rem] h-[12.3rem] mt-[0.5rem] rounded-lg bg-gradient-to-b from-degradeUm to-degradeDois tablet:mt-0 tablet:w-[20.8rem] tablet:h-[22.5rem] tablet:rounded-xl">               
+                                <Image src={rede} alt='' className='mx-auto pt-[1.9rem] tablet:w-[7.8rem] tablet:h-auto tablet:mt-[2rem]'/>
+                                <div className="text-fundo text-[1.25rem] font-extrabold flex justify-center mt-[1.5rem] tablet:text-[3.12rem]">
                                     <p>+</p>
                                     <Card initialValue={450} finalValue={500}></Card>     
                                 </div>
-                                <p className='text-[0.8rem] text-fundo flex mx-auto justify-center mt-[0.5rem]'>REDE DE CLIENTES</p>
+                                <p className='text-[0.8rem] text-fundo flex mx-auto justify-center mt-[0.5rem] tablet:text-[1.5rem]'>REDE DE CLIENTES</p>
                     </div>
                 </div>         
             </div>
