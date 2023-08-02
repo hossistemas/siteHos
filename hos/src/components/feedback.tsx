914693751833a083b888e4e37e4f6d3639c3a569
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-02 19:59:46 -0300

    Iniciado alterações na logica e estilo do componente feedback

--- hos/src/components/feedback.tsx
+++ hos/src/components/feedback.tsx
@@ -10,12 +10,30 @@ interface FeedbackProps {
 
 
 const images = [
-    '/geraldo.png',
-     '/luciana.png',
-   '/marilva.png',
-    '/roberto.png',
-    '/rodrigo.png',
-    '/valfarma.png'
+    { image:'/estrelas.svg',
+    title: 'Valfarma',
+    text: 'Venda com inteligência e rentabilidade. Encontre alternativas mais rentáveis ao consultar um produto e aumente sua margem de lucro.'
+    },
+    { image:'/estrelas.svg',
+    title: 'Maximize sua margem de lucro',
+    text: 'Venda com inteligência e rentabilidade. Encontre alternativas mais rentáveis ao consultar um produto e aumente sua margem de lucro.'
+    },
+    { image:'/estrelas.svg',
+    title: 'Maximize sua margem de lucro',
+    text: 'Venda com inteligência e rentabilidade. Encontre alternativas mais rentáveis ao consultar um produto e aumente sua margem de lucro.'
+    },
+    { image:'/estrelas.svg',
+    title: 'Maximize sua margem de lucro',
+    text: 'Venda com inteligência e rentabilidade. Encontre alternativas mais rentáveis ao consultar um produto e aumente sua margem de lucro.'
+    },
+    { image:'/estrelas.svg',
+    title: 'Maximize sua margem de lucro',
+    text: 'Venda com inteligência e rentabilidade. Encontre alternativas mais rentáveis ao consultar um produto e aumente sua margem de lucro.'
+    },
+    { image:'/estrelas.svg',
+    title: 'Maximize sua margem de lucro',
+    text: 'Venda com inteligência e rentabilidade. Encontre alternativas mais rentáveis ao consultar um produto e aumente sua margem de lucro.'
+    },
     
   ];
 
@@ -54,11 +72,13 @@ export function Feedback(props: FeedbackProps){
                 <h2>Já ajudamos <span className='text-vermelho'> centenas de farmácias</span> e drogarias a descomplicar suas rotinas. </h2>
         </div>
 
-      <div className="carousel overflow-hidden tablet:w-[1200px] flex mx-auto">
-        <div className="carousel-slides flex transition-transform ease-in-out duration-300 tablet:w-1/3" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
-          {images.map((image, index) => (
-            <div key={index} className={`carousel-slide flex-shrink-0 w-full ${index === currentIndex ? 'active' : ''}`}>
-              <Image src={image} alt={`Image ${index + 1}`} width={250} height={210} className='flex mx-auto tablet:w-[25rem]'/>
+        <div className="carousel overflow-hidden  flex mx-auto h-[19rem] tablet:h-[26rem] tablet:w-[1090px] ">
+        <div className="carousel-slides flex transition-transform ease-in-out duration-300  tablet:w-1/3" style={{ transform: `translateX(-${currentIndex *(100 / itemsPerSlide)}%)` }}>
+          {images.map((item, index) => (
+            <div key={index} className={`carousel-slide flex-shrink-0 w-[16rem] h-[15rem] mb-[5rem]  mx-[0.8rem] justify-center bg-white  drop-shadow-lg rounded-xl tablet:w-[21.1rem] tablet:h-[25.4rem] ${index === currentIndex ? 'active' : ''}`}>
+              <Image src={item.image} alt={`Image ${index + 1}`} width={70} height={100} className='flex mx-auto pt-[2rem]  tablet:w-[6rem] tablet:pt-[3rem] tablet:pb-[2rem]'/>
+              <h2 className='text-center text-[1rem] text-grafite font-bold tablet:text-[1.25rem] tablet:px-[1.5rem] tablet:pb-[1.3rem]'>{item.title}</h2>
+              <p className='text-center text-[0.8rem] text-grafite tablet:text-[1rem] tablet:w-[18rem] tablet:flex tablet:mx-auto'>{item.text}</p>
             </div>
           ))}
         </div>
