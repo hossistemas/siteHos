86a77fb43b5ea18856804b465c701ae01a509e5e
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-02 19:47:07 -0300

    Finalizado responsividade componente Sistema

--- hos/src/components/sistema.tsx
+++ hos/src/components/sistema.tsx
@@ -102,13 +102,13 @@ export function Sistema(props: SistemaProps) {
             <div key={index} className={`carousel-slide flex-shrink-0 w-[16rem] h-[15rem] mb-[5rem]  mx-[0.8rem] justify-center bg-white  drop-shadow-lg rounded-xl tablet:w-[21.1rem] tablet:h-[25.4rem] ${index === currentIndex ? 'active' : ''}`}>
               <Image src={item.image} alt={`Image ${index + 1}`} width={70} height={100} className='flex mx-auto pt-[2rem]  tablet:w-[6rem] tablet:pt-[3rem] tablet:pb-[2rem]'/>
               <h2 className='text-center text-[1rem] text-grafite font-bold tablet:text-[1.25rem] tablet:px-[1.5rem] tablet:pb-[1.3rem]'>{item.title}</h2>
-              <p className='text-center text-[0.8rem] text-grafite tablet:text-[1rem]'>{item.text}</p>
+              <p className='text-center text-[0.8rem] text-grafite tablet:text-[1rem] tablet:w-[18rem] tablet:flex tablet:mx-auto'>{item.text}</p>
             </div>
           ))}
         </div>
       </div>
 
-      <div className="carousel-pagination  flex justify-center -mt-[2rem] tablet:visible tablet:flex tablet:justify-center">
+      <div className="carousel-pagination  flex justify-center -mt-[2rem] tablet:visible tablet:flex tablet:justify-center tablet:mt-2">
         {Array.from({ length: totalPages }, (_, index) => index).map((page) => (
           <button
             key={page}
@@ -120,18 +120,18 @@ export function Sistema(props: SistemaProps) {
         ))}
       </div>
 
-        <div className="flex justify-center mt-[0.5rem] tablet:visible tablet:flex tablet:justify-center tablet:mt-[1rem]">
+        <div className="flex justify-center mt-[0.5rem] tablet:visible ">
         <button
-            className=" bg-fundo mr-[1rem] border-2 rounded-full text-[24px] px-[0.5rem] py-[1rem] cursor-pointer hover:px-[0.6rem] hover:py-[1.1rem] hover:bg-magenta hover:transition hover:ease-in hover:duration-300 hover:border-0"
+            className=" bg-fundo tablet:bg-none mr-[1rem] border-2 tablet:border-none rounded-full text-[24px] px-[0.5rem] py-[1rem] cursor-pointer hover:px-[0.6rem] hover:py-[1.1rem] hover:bg-magenta hover:transition hover:ease-in hover:duration-300 hover:border-0"
             onClick={prevSlide}
             >
-            <Image src={arrowRight} alt='' className='rotate-180 w-[1rem] h-[1rem] mx-[0.4rem] hover:filter hover:grayscale '/>
+            <Image src={arrowRight} alt='' className='rotate-180 w-[1rem] tablet:w-[2rem] tablet:h-[2rem] h-[1rem] mx-[0.4rem] hover:filter hover:grayscale '/>
             </button>
             <button
-            className=" bg-fundo  border-2 rounded-full text-[24px] px-[0.5rem] py-[1rem] cursor-pointer  hover:px-[0.6rem] hover:py-[1.1rem] hover:bg-magenta hover:hover:transition hover:ease-in hover:duration-300 hover:border-0"
+            className=" bg-fundo tablet:bg-none  tablet:px-0 tablet:py-0 border-2  tablet:border-none rounded-full text-[24px] px-[0.5rem] py-[1rem] cursor-pointer  hover:px-[0.6rem] hover:py-[1.1rem] hover:bg-magenta hover:hover:transition hover:ease-in hover:duration-300 hover:border-0"
             onClick={nextSlide}
             >
-            <Image src={arrowRight} alt='' className='w-[1rem] h-[1rem] mx-[0.4rem] '/>
+            <Image src={arrowRight} alt='' className='w-[1rem] h-[1rem] tablet:w-[2rem] tablet:h-[2rem] mx-[0.4rem] '/>
             </button>
         </div>
 
