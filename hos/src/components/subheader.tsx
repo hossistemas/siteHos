c58b2483c2fed3b710cd982e7b112a268fe0dfa0
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-05 13:20:01 -0300

    Responsividade ajustada do componente CTA

---
+++ hos/src/components/subheader.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
+
+interface SubheaderProps {
+
+}
+
+export function Subheader(props: SubheaderProps){
+
+    return(
+        <>
+       
+
+        </>
+ 
+    )
+            }
