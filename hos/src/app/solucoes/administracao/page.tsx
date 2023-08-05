6a2b79c66239c28f038e55b801961ac156ec875a
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-05 15:08:39 -0300

    Iniciado componente otimize

--- hos/src/app/solucoes/administracao/page.tsx
+++ hos/src/app/solucoes/administracao/page.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/components/header";
 import { HeroSection } from "@/components/herosection";
 import { Subheader } from "@/components/subheader";
 import gerenciamento from '../../../../public/gerenciamento.png'
+import { Otimize } from "@/components/otimize";
 
 export default function Administracao() {
   return(
@@ -21,7 +22,8 @@ export default function Administracao() {
       textClassName={"text-grafite"}
       buttonClassName={"bg-gradient-to-b from-laranja to-magenta text-white hover:bg-gradient-to-b hover:from-darker hover:to-darker hover:transition hover:duration-500"}
       img={gerenciamento}
-    />  
+      /> 
+      <Otimize /> 
   
       </>
    )
