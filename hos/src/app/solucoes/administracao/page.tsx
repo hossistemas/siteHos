1fcd1192d7491638c411d3b160b61698d1bee9ce
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-05 14:50:10 -0300

    Finalizado hero section

--- hos/src/app/solucoes/administracao/page.tsx
+++ hos/src/app/solucoes/administracao/page.tsx
@@ -1,5 +1,7 @@
 import { Header } from "@/components/header";
+import { HeroSection } from "@/components/herosection";
 import { Subheader } from "@/components/subheader";
+import gerenciamento from '../../../../public/gerenciamento.png'
 
 export default function Administracao() {
   return(
@@ -11,7 +13,15 @@ export default function Administracao() {
       terceiro={"Processos de Manipulação"} hrefTerceiro={""}
       quarto={"Soluções complementares"}  hrefQuarto={""}
       />
-        
+      <HeroSection 
+      bgClassName={"bg-fundo"}
+      title={"Melhore sua margem de lucro em cada oportunidade de venda"} 
+      titleClassName={"text-grafite"}
+      text={"Velocidade e entendimento do cliente como chave para o seu sucesso."} 
+      textClassName={"text-grafite"}
+      buttonClassName={"bg-gradient-to-b from-laranja to-magenta text-white hover:bg-white hover:text-darker hover:transition hover:duration-500"}
+      img={gerenciamento}
+    />  
   
       </>
    )
