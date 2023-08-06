87357a64c72eb966c153d77769c111a746b2c6ac
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-06 08:29:16 -0300

    Iniciado pagina complementares

---
+++ hos/src/app/solucoes/complementares/page.tsx
@@ -0,0 +1,33 @@
+import { Header } from "@/components/header";
+import { HeroSection } from "@/components/herosection";
+import { Subheader } from "@/components/subheader";
+import complementares from '../../../../public/complementares.png'
+ 
+import { Cta } from "@/components/cta";
+import { Footer } from "@/components/footer";
+
+export default function Administracao() {
+  return(
+      <>
+      <Header />
+      <Subheader
+      primeiro={"Administração"} hrefPrimeiro={"/solucoes/administracao"}
+      segundo={"Gerenciamento de Loja"}  hrefSegundo={"/solucoes/gerenciamento"}
+      terceiro={"Processos de Manipulação"} hrefTerceiro={"/solucoes/processos"}
+      quarto={"Soluções complementares"}  hrefQuarto={"/solucoes/complementares"}
+      />
+      <HeroSection 
+      bgClassName={"bg-fundo"}
+      title={"Aprimore sua experiência e torne-a ainda mais completa"} 
+      titleClassName={"text-grafite"}
+      text={"Centralize e automatize as informações da sua farmácia de maneira fácil, e rápida."} 
+      textClassName={"text-grafite"}
+      buttonClassName={"bg-gradient-to-b from-laranja to-magenta text-white hover:bg-gradient-to-b hover:from-darker hover:to-darker hover:transition hover:duration-500"}
+      img={complementares}
+      /> 
+      <Cta />
+      <Footer />
+  
+      </>
+   )
+  }
--- hos/src/components/emuitomais.tsx
+++ hos/src/components/emuitomais.tsx
@@ -11,16 +11,17 @@ export function Emuitomais(props: EmuitomaisProps){
         <>
         <div className="px-[2.5rem] py-[2rem] bg-fundo tablet:pt-[6rem] tablet:px-[8rem]">
             <div className="max-w-[460px] mx-auto tablet:max-w-[1200px] tablet:flex tablet:justify-center tablet:items-center tablet:mx-auto">
-            <h2 className="text-[1.5rem] font-bold text-grafite leading-tight mb-[1.5rem] tablet:text-[2rem]">E muito mais...</h2>
+    
                 <div className="pb-[1.7rem] tablet:mr-[4rem]">
                     <Image src={emuitomais} alt="" className="max-w-[17rem] h-auto mx-auto tablet:max-w-[33rem] tablet:h-auto"/>
                 </div>
 
                 <div className="max-w-[17rem] mx-auto tablet:max-w-[27rem]">
                     
-                    <ul className="list-disc text-[0.8rem]  text-grafite  font-medium ml-[1rem] leading-6 tablet:text-[1.25rem] tablet:leading-normal">
+                    <ul className="list-disc text-[0.8rem]  text-grafite  font-medium ml-[1rem] leading-6 tablet:-mt-[4rem] tablet:text-[1.25rem] tablet:leading-normal">
+                        <h2 className="text-[1.5rem] font-bold text-grafite leading-tight mb-[1.5rem] tablet:text-[2rem] ">E muito mais...</h2>
                         <li>Tela inicial de alerta da validade dos lotes</li>
-                        <li>Emissão de orçamentos com impressão de documentos relacionadosEmissão de orçamentos com impressão de documentos relacionados</li>
+                        <li>Emissão de orçamentos com impressão de documentos relacionados</li>
                         <li>Formação de preço de medicamentos homeopáticos considerando insumos internos e/ou industrializados</li>
                         <li>Definição automática da embalagem</li>
                         <li>Controle de estoque e laudos por lote, com geração automática do fator de correção</li>
