a10e08030e27389949d34d24e7f5f16a88f13998
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-12 13:53:22 -0300

    Iniciado responsividade componente converse

--- hos/src/components/converse.tsx
+++ hos/src/components/converse.tsx
@@ -26,16 +26,16 @@ export function Formulario () {
 
     return (
         <div className="bg-fundo">
-             <div className="bg-gradient-to-b from-laranja to-magenta w-auto h-[16rem] pt-[3rem] tablet:h-[21rem]">
-                <div className="ml-[3rem] tablet:flex tablet:justify-center tablet:items-center tablet:pt-[4.5rem]">
-                    <h4 className="text-[1.25rem] font-bold text-white w-[12.5rem] leading-tight">Converse com um de nossos consultores</h4>
-                    <div className="bg-white w-[3rem] h-[0.1rem] my-[0.5rem]"/>
-                    <p className="text-[0.8rem] text-white font-medium w-[12.5rem] leading-tight">Você ainda tem alguma dúvida? Entre em contato conosco pelos canais oficiais.</p>
+             <div className="bg-gradient-to-b from-laranja to-magenta w-auto h-[16rem] pt-[3rem] tablet:h-[24rem]">
+                <div className="ml-[3rem]  tablet:pt-[3rem] tablet:ml-[10rem]">
+                    <h4 className="text-[1.25rem] font-bold text-white w-[12.5rem] leading-tight tablet:text-[2.75rem] tablet:w-[29rem]">Converse com um de nossos consultores</h4>
+                    <div className="bg-white w-[3rem] h-[0.1rem] my-[0.5rem] tablet:hidden"/>
+                    <p className="text-[0.8rem] text-white font-medium w-[12.5rem] leading-tight tablet:text-[1.25rem] tablet:w-[29rem] tablet:pt-[3rem]">Você ainda tem alguma dúvida? Entre em contato conosco pelos canais oficiais.</p>
                 </div>
             </div>
             
-            
-            <form action="" className="bg-white mx-auto w-[22rem] h-[48rem] rounded-lg drop-shadow-botao mt-[-2rem]">
+    <div className="tablet:grid tablet:grid-cols-2">
+            <form action="" className="bg-white mx-auto w-[22rem] h-[48rem] rounded-lg drop-shadow-botao mt-[-2rem] tablet:col-start-2">
                 <div className="mx-[1rem] py-[2rem] grid grid-cols-2">
                     <label className="nome text-[0.875rem] text-cinza font-bold col-span-2" >Nome completo*
                         <input type="text" placeholder="Digite aqui seu nome completo" className="w-[20rem] h-[2rem] border-2 rounded-lg font-normal text-[0.7rem] pl-[0.7rem] mt-[0.3rem] mb-[1.5rem]" />
@@ -90,41 +90,42 @@ export function Formulario () {
                 </div>
             </form>
         
-
-        <div className="pt-[5rem]">
-            <div className='w-[2.5rem] h-[2.5rem] flex items-center justify-center mx-auto bg-gradient-to-b from-laranja to-magenta rounded-full '>
-                <Image src={telefone} alt="" />
+        <div className="tablet:col-start-1">
+            <div className="pt-[5rem]">
+                <div className='w-[2.5rem] h-[2.5rem] flex items-center justify-center mx-auto bg-gradient-to-b from-laranja to-magenta rounded-full '>
+                    <Image src={telefone} alt="" />
+                </div>
+                    <h5 className="text-grafite text-[0.9rem] w-[10rem] font-semibold mx-auto text-center mt-[0.6rem]" >Telefone</h5>
+                    <p className="text-cinza text-[0.9rem] w-[10rem] font-semibold text-center mx-auto mt-[-0.3rem]">+55 (54) 2621-4400</p>
             </div>
-                <h5 className="text-grafite text-[0.9rem] w-[10rem] font-semibold mx-auto text-center mt-[0.6rem]" >Telefone</h5>
-                <p className="text-cinza text-[0.9rem] w-[10rem] font-semibold text-center mx-auto mt-[-0.3rem]">+55 (54) 2621-4400</p>
+
+            <div className="pt-[2.5rem]">
+                <div className='w-[2.5rem] h-[2.5rem] flex items-center justify-center mx-auto bg-gradient-to-b from-laranja to-magenta rounded-full '>
+                    <Image src={whatsapp} alt="" />
+                </div>
+                    <h5 className="text-grafite text-[0.9rem] w-[10rem] font-semibold mx-auto text-center mt-[0.6rem]" >WhatsApp</h5>
+                    <p className="text-cinza text-[0.9rem] w-[10rem] font-semibold text-center mx-auto mt-[-0.3rem]">+55 (54) 9 9640 0699</p>
+            </div>
+
+            <div className="pt-[2.5rem]">
+                <div className='w-[2.5rem] h-[2.5rem] flex items-center justify-center mx-auto bg-gradient-to-b from-laranja to-magenta rounded-full '>
+                    <Image src={horario} alt="" />
+                </div>
+                    <h5 className="text-grafite text-[0.9rem] w-[10rem] font-semibold mx-auto text-center mt-[0.6rem]" >Horário de atendimento</h5>
+                    <p className="text-cinza text-[0.9rem] w-[10rem] font-semibold text-center mx-auto mt-[-0.3rem] leading-tight">Segunda à Sexta-feira: 08h às 18h</p>
+            </div>
+
+            <div className="pt-[2.5rem] pb-[3rem]">
+                <div className='w-[2.5rem] h-[2.5rem] flex items-center justify-center mx-auto bg-gradient-to-b from-laranja to-magenta rounded-full '>
+                    <Image src={localizacao} alt="" />
+                </div>
+                    <h5 className="text-grafite text-[0.9rem] w-[10rem] font-semibold mx-auto text-center mt-[0.6rem]" >Endereço</h5>
+                    <p className="text-cinza text-[0.9rem] w-[12.2rem] font-semibold text-center mx-auto mt-[-0.3rem] leading-tight">Rua Pernambuco, 212, Bairro Humaitá, Bento Gonçalves RS - 95705-024</p>
+            </div>
         </div>
 
-        <div className="pt-[2.5rem]">
-            <div className='w-[2.5rem] h-[2.5rem] flex items-center justify-center mx-auto bg-gradient-to-b from-laranja to-magenta rounded-full '>
-                <Image src={whatsapp} alt="" />
-            </div>
-                <h5 className="text-grafite text-[0.9rem] w-[10rem] font-semibold mx-auto text-center mt-[0.6rem]" >WhatsApp</h5>
-                <p className="text-cinza text-[0.9rem] w-[10rem] font-semibold text-center mx-auto mt-[-0.3rem]">+55 (54) 9 9640 0699</p>
-        </div>
 
-        <div className="pt-[2.5rem]">
-            <div className='w-[2.5rem] h-[2.5rem] flex items-center justify-center mx-auto bg-gradient-to-b from-laranja to-magenta rounded-full '>
-                <Image src={horario} alt="" />
-            </div>
-                <h5 className="text-grafite text-[0.9rem] w-[10rem] font-semibold mx-auto text-center mt-[0.6rem]" >Horário de atendimento</h5>
-                <p className="text-cinza text-[0.9rem] w-[10rem] font-semibold text-center mx-auto mt-[-0.3rem] leading-tight">Segunda à Sexta-feira: 08h às 18h</p>
-        </div>
-
-        <div className="pt-[2.5rem] pb-[3rem]">
-            <div className='w-[2.5rem] h-[2.5rem] flex items-center justify-center mx-auto bg-gradient-to-b from-laranja to-magenta rounded-full '>
-                <Image src={localizacao} alt="" />
-            </div>
-                <h5 className="text-grafite text-[0.9rem] w-[10rem] font-semibold mx-auto text-center mt-[0.6rem]" >Endereço</h5>
-                <p className="text-cinza text-[0.9rem] w-[12.2rem] font-semibold text-center mx-auto mt-[-0.3rem] leading-tight">Rua Pernambuco, 212, Bairro Humaitá, Bento Gonçalves RS - 95705-024</p>
-        </div>
-
-
-        <div>
+        <div className="tablet:col-star-1">
             <p className="text-[0.9rem] text-grafite text-center mx-auto font-semibold">Siga-nos nas redes sociais!</p>
             <div className="flex justify-center items-center pt-[1rem] pb-[3rem] gap-x-6 tablet:justify-end tablet:pb-[3rem]">
                     <Image src={facebook} alt=""/>
@@ -134,6 +135,7 @@ export function Formulario () {
             </div>
         </div>    
 
+    </div>  
 </div>
          
     )
