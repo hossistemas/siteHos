b294aba74895b1111fabc53186d107a86c756c29
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-16 20:02:28 -0300

    Adicionado links ao componente videos

--- hos/src/components/videos.tsx
+++ hos/src/components/videos.tsx
@@ -3,6 +3,7 @@ import indicadores from '../../public/indicadores.png'
 import dicas from '../../public/dicas.png'
 import prosperar from '../../public/prosperar.png'
 import frente from '../../public/frente.png'
+import Link from 'next/link'
 
 interface VideosProps {
 
@@ -17,22 +18,30 @@ export function Videos(props : VideosProps){
 
             <div className='tablet:grid tablet:grid-cols-2 tablet:gap-y-10 tablet:pb-[6rem]'>
                 <div className='mx-auto'>
-                    <Image src={indicadores} alt='' className='mx-auto tablet:w-[35rem]' />
+                    <Link href='https://www.youtube.com/watch?v=_l5KEDfDiDc&ab_channel=HosSistemas' target='_blank'>
+                        <Image src={indicadores} alt='' className='mx-auto tablet:w-[35rem]' />
+                    </Link>
                     <p className='text-center text-[0.87rem] font-semibold mx-auto tablet:text-[1.5rem] tablet:w-[24rem]'>Indicadores na Gestão de Drogarias</p>
                 </div>
 
                 <div className='mx-auto py-[2rem] tablet:py-0'>
-                    <Image src={dicas} alt='' className='mx-auto tablet:w-[35rem]' />
+                    <Link href='https://www.youtube.com/watch?v=lm1aYxNGV3E&ab_channel=HosSistemas' target='_blank'>
+                        <Image src={dicas} alt='' className='mx-auto tablet:w-[35rem]' />
+                    </Link>
                     <p className='text-center text-[0.87rem] font-semibold w-[14rem] mx-auto tablet:text-[1.5rem] tablet:w-[24rem]'>Dicas de Gestão Farmacêutica para o Sucesso em Farmácias</p>
                 </div>
 
                 <div className='mx-auto'>
-                    <Image src={prosperar} alt='' className='mx-auto tablet:w-[35rem]' />
+                    <Link href='https://www.youtube.com/watch?v=gyJnJdlnHQI&ab_channel=HosSistemas' target='_blank'>
+                        <Image src={prosperar} alt='' className='mx-auto tablet:w-[35rem]' />
+                    </Link>
                     <p className='text-center text-[0.87rem] font-semibold w-[14rem] mx-auto tablet:text-[1.5rem] tablet:w-[24rem]'>Como prosperar no Varejo Farmacêutico</p>
                 </div>
 
                 <div className='mx-auto py-[2rem] tablet:py-0 tablet:mt-[-2.3rem]'>
+                <Link href='https://www.youtube.com/watch?v=RGg3OHoguWo&ab_channel=HosSistemas' target='_blank'>
                     <Image src={frente} alt='' className='mx-auto tablet:w-[35rem]' />
+                </Link>
                     <p className='text-center text-[0.87rem] font-semibold w-[14rem] mx-auto tablet:text-[1.5rem] tablet:w-[24rem]'>HOS Frente de Loja</p>
                 </div>
             </div>
