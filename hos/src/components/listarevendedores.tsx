097183012e16e6325538a4890ea2d9b510323c1b
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-19 11:24:31 -0300

    Iniciado logica revendedores

--- hos/src/components/listarevendedores.tsx
+++ hos/src/components/listarevendedores.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 
 import React, { useState } from 'react';
 
@@ -17,158 +17,133 @@ interface ListaProps {
 
 export function Lista(props: ListaProps) {      
 
-    const [filterText, setFilterText] = useState('');
+    const [filterValue, setFilterValue] = useState('');
 
     const revendas: any[] = [
         {
-            image:'/maletinha.svg',
             title:'Inforsas',
             cidade:'Arapiraca - Alagoas',
-            email:'inforsas@gmail.com',
-            telefone:'(84) 9.9697-1337',
+           
         },
         {
-            image:'/maletinha.svg',
             title:'Consultime Soluções',
             cidade:'Salvador - Bahia',
-            email:'consultimesolucoes@gmail.com',
-            telefone:'(71) 3372-4545',
         },
         {
-            image:'/maletinha.svg',
             title:'Contabilize Gestão',
             cidade:'Castelo - Espírito Santo',
             email:'nubia@contabilizegestao.com.br',
             telefone:'(26) 3542-5165',
         },
         {
-            image:'/maletinha.svg',
             title:'Rafael Lucena Consultoria',
             cidade:'Guarapari - Espírito Santo',
             email:'rafaellucenaconsultoria@gmail.com',
             telefone:'(27) 9.9652-1589',
         },
         {
-            image:'/maletinha.svg',
             title:'Lef Sistemas',
             cidade:'Várzea Grande - Mato Grosso',
             email:'ilson.ramos@lefsistemas.com.br',
             telefone:'(65) 3044-3573',
         },
         {
-            image:'/maletinha.svg',
             title:'Andrade Silva Tecnologia',
             cidade:'Campo Grande - Mato Grosso do Sul',
             email:'paulo@infoin.com.br',
             telefone:'(67) 9.9129-0789',
         },
         {
-            image:'/maletinha.svg',
             title:'Consulfar',
             cidade:'Belo Horizonte - Minas Gerais',
             email:'jadersondsousa@gmail.com',
             telefone:'(31) 9.8480-7777',
         },
         {
-            image:'/maletinha.svg',
             title:'Softek Automação',
             cidade:'Governador Valadares - Minas Gerais',
             email:'softekautomacao@gmail.com',
             telefone:'(33) 3271-4427',
         },
         {
-            image:'/maletinha.svg',
             title:'Pablo Gabriel Teixeira da Silva',
             cidade:'Monte Azul - Minas Gerais',
             email:'hosbh@hotmail.com',
             telefone:'(38) 3811-1908',
         },
         {
-            image:'/maletinha.svg',
             title:'Relcach',
             cidade:'Patos de Minas - Minas Gerais',
             email:'realch@hotmail.com',
             telefone:'(34) 3061-5506',
         },
         {
-            image:'/maletinha.svg',
             title:'Unisystem',
             cidade:'Ananindeua - Pará',
             email:'marcos.lobato1840@gmail.com',
             telefone:'(91) 9.8048-5701',
         },
         {
-            image:'/maletinha.svg',
             title:'Integra Sistemas',
             cidade:'Santo André - Paraíba',
             email:'auxiliar.adm@integrasistemas.com.br',
             telefone:'(11) 4475.3712',
         },
         {
-            image:'/maletinha.svg',
             title:'Inovar Automação',
             cidade:'Sapé - Paraíba',
             email:'zairofs@hotmail.com',
             telefone:'(11) 4475.3712',
         },
         {
-            image:'/maletinha.svg',
             title:'Laz Digital',
             cidade:'Sertânia - Pernambucoa',
             email:'zuleilson@hotmail.com',
             telefone:'(87) 9.9647-8780',
         },
         {
-            image:'/maletinha.svg',
             title:'Sysfarma',
             cidade:'Belford Roxo - Rio de Janeiro',
             email:'alessandro.lucc@gmail.com',
             telefone:'(21) 2761-1494',
         },
         {
-            image:'/maletinha.svg',
             title:'Revenda Giovani',
             cidade:'Duque de Caxias - Rio de Janeiro',
             email:'giovaniaraujo@hotmail.com',
             telefone:'(21) 9.8001-5036',
         },
         {
-            image:'/maletinha.svg',
             title:'Invicttus',
             cidade:'Natal - Rio Grande do Norte',
             email:'andrearaujo@matecnologia.com.br',
             telefone:'(84) 9.9118-7859',
         },
         {
-            image:'/maletinha.svg',
             title:'Cleonides Fernandes de Brito',
             cidade:'Parnamirim - Rio Grande do Norte',
             email:'cleonidesjr@hotmail.com',
             telefone:'(84) 9.9630-5323',
         },
         {
-            image:'/maletinha.svg',
             title:'Zupa System',
             cidade:'Capão da Canoa - Rio Grande do Sul',
             email:'zupasystem@gmail.com',
             telefone:'(51) 3622-1509',
         },
         {
-            image:'/maletinha.svg',
             title:'BeEasy',
             cidade:'Farroupilha - Rio Grande do Sul',
             email:'pri@beeasy.com.br',
             telefone:'(54) 3698-9128',
         },
         {
-            image:'/maletinha.svg',
             title:'Celulah',
             cidade:'Igrejinha - Rio Grande do Sul',
             email:'admcelulah@gmail.com',
             telefone:'(54) 9.9697-0208',
         },
         {
-            image:'/maletinha.svg',
             title:'Base4 Sistemas Ltda',
             cidade:'Catanduva - São Paulo',
             email:'fernandofarinha@base4.com.br',
@@ -176,14 +151,13 @@ export function Lista(props: ListaProps) {
         },
     ];
   
-    const filteredRevendas = revendas.filter(revenda =>
-      revenda.nome.toLowerCase().includes(filterText.toLowerCase()) ||
-      revenda.cidade.toLowerCase().includes(filterText.toLowerCase())
-    );
-
-    const handleFilterChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-        setFilterText(event.target.value);
-      };
+    const filteredData = revendas.filter(item => {
+        const itemTitle = (item.title ?? '').toLowerCase();
+        const itemCidade = (item.cidade ?? '').toLowerCase();
+        const filterText = filterValue.toLowerCase();
+        
+        return itemTitle.includes(filterText) || itemCidade.includes(filterText);
+    });
 
         return(
 
@@ -194,9 +168,10 @@ export function Lista(props: ListaProps) {
                     <h3 className="text-grafite w-[15.5rem] text-[1.4rem] font-bold pb-[1.5rem] mx-auto">Encontre uma revenda próxima a você!</h3>
                     <input
                     type="text"
+                    value={filterValue}
                     placeholder="Local ou revendedor"
                     className="w-[15rem] h-[2.3rem] flex justify-center mx-auto drop-shadow-lg pl-[1rem] rounded-xl text-[0.7rem] text-cinza"
-                    onChange={handleFilterChange}
+                    onChange={e => setFilterValue(e.target.value)}
                     />
                 </div>
             <Swiper
@@ -212,11 +187,11 @@ export function Lista(props: ListaProps) {
                     modules={[Pagination, Scrollbar]}
                     className="mySwiper w-[500px] h-[1000px] bg-red-500"
                 >
-                    {filteredRevendas.map(revenda => (
-                        <><SwiperSlide key={revenda.nome}>
-                            <div className="pb-[5rem] w-[15.5rem] h-[8.5rem] my-[0.7rem] mx-auto bg-white drop-shadow-xl rounded-lg">
+                    {filteredData.map(item => (
+                        <><SwiperSlide >
+                            <div key={item.id} className="pb-[5rem] w-[15.5rem] h-[8.5rem] my-[0.7rem] mx-auto bg-white drop-shadow-xl rounded-lg">
                                 <Image src={maletinha} alt='' className="flex w-[1rem] h-[2rem] ml-[1.35rem] pt-[1rem]" />
-                                <h2 className="text-[0.87rem] text-grafite font-semibold py-[0.8rem] ml-[1.35rem]">Inforsas</h2>
+                                <h2  className="text-[0.87rem] text-grafite font-semibold py-[0.8rem] ml-[1.35rem]">Inforsas</h2>
                                 <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">Cidade:</span> Arapiraca - Alagoas </p>
                                 <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">E-mail:</span> inforsas@gmail.com </p>
                                 <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">Telefone:</span> (84) 9.9697-1337 </p>
@@ -224,7 +199,7 @@ export function Lista(props: ListaProps) {
                         </SwiperSlide>
                         
                         <SwiperSlide>
-                                <div className="pb-[5rem] w-[15.5rem] h-[8.5rem] my-[0.7rem] mx-auto bg-white drop-shadow-xl rounded-lg">
+                                <div key={item.id}className="pb-[5rem] w-[15.5rem] h-[8.5rem] my-[0.7rem] mx-auto bg-white drop-shadow-xl rounded-lg">
                                     <Image src={maletinha} alt='' className="flex w-[1rem] h-[2rem] ml-[1.35rem] pt-[1rem]" />
                                     <h2 className="text-[0.87rem] text-grafite font-semibold py-[0.8rem] ml-[1.35rem]">Consultime Soluções</h2>
                                     <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">Cidade:</span> Salvador - Bahia</p>
@@ -361,7 +336,9 @@ export function Lista(props: ListaProps) {
                                     <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">E-mail:</span> alessandro.lucc@gmail.com </p>
                                     <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">Telefone:</span> (21) 2761-1494</p>
                                 </div>
-                            </SwiperSlide><SwiperSlide>
+                            </SwiperSlide>
+                            
+                            <SwiperSlide>
                                 <div className="pb-[5rem] w-[15.5rem] h-[8.5rem] my-[0.7rem] mx-auto bg-white drop-shadow-xl rounded-lg">
                                     <Image src={maletinha} alt='' className="flex w-[1rem] h-[2rem] ml-[1.35rem] pt-[1rem]" />
                                     <h2 className="text-[0.87rem] text-grafite font-semibold py-[0.8rem] ml-[1.35rem]">Revenda Giovani</h2>
@@ -369,7 +346,9 @@ export function Lista(props: ListaProps) {
                                     <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">E-mail:</span> giovaniaraujo@hotmail.com </p>
                                     <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">Telefone:</span> (21) 9.8001-5036</p>
                                 </div>
-                            </SwiperSlide><SwiperSlide>
+                            </SwiperSlide>
+                            
+                            <SwiperSlide>
                                 <div className="pb-[5rem] w-[15.5rem] h-[8.5rem] my-[0.7rem] mx-auto bg-white drop-shadow-xl rounded-lg">
                                     <Image src={maletinha} alt='' className="flex w-[1rem] h-[2rem] ml-[1.35rem] pt-[1rem]" />
                                     <h2 className="text-[0.87rem] text-grafite font-semibold py-[0.8rem] ml-[1.35rem]">Invicttus</h2>
@@ -377,7 +356,9 @@ export function Lista(props: ListaProps) {
                                     <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">E-mail:</span> andrearaujo@matecnologia.com.br </p>
                                     <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">Telefone:</span> (84) 9.9118-7859</p>
                                 </div>
-                            </SwiperSlide><SwiperSlide>
+                            </SwiperSlide>
+                            
+                            <SwiperSlide>
                                 <div className="pb-[5rem] w-[15.5rem] h-[8.5rem] my-[0.7rem] mx-auto bg-white drop-shadow-xl rounded-lg">
                                     <Image src={maletinha} alt='' className="flex w-[1rem] h-[2rem] ml-[1.35rem] pt-[1rem]" />
                                     <h2 className="text-[0.87rem] text-grafite font-semibold py-[0.8rem] ml-[1.35rem]">Cleonides Fernandes de Brito</h2>
@@ -385,7 +366,9 @@ export function Lista(props: ListaProps) {
                                     <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">E-mail:</span> cleonidesjr@hotmail.com </p>
                                     <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">Telefone:</span> (84) 9.9630-5323</p>
                                 </div>
-                            </SwiperSlide><SwiperSlide>
+                            </SwiperSlide>
+                            
+                            <SwiperSlide>
                                 <div className="pb-[5rem] w-[15.5rem] h-[8.5rem] my-[0.7rem] mx-auto bg-white drop-shadow-xl rounded-lg">
                                     <Image src={maletinha} alt='' className="flex w-[1rem] h-[2rem] ml-[1.35rem] pt-[1rem]" />
                                     <h2 className="text-[0.87rem] text-grafite font-semibold py-[0.8rem] ml-[1.35rem]">Zupa System</h2>
@@ -393,7 +376,9 @@ export function Lista(props: ListaProps) {
                                     <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">E-mail:</span> zupasystem@gmail.com </p>
                                     <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">Telefone:</span> (51) 3622-1509</p>
                                 </div>
-                            </SwiperSlide><SwiperSlide>
+                            </SwiperSlide>
+                            
+                            <SwiperSlide>
                                 <div className="pb-[5rem] w-[15.5rem] h-[8.5rem] my-[0.7rem] mx-auto bg-white drop-shadow-xl rounded-lg">
                                     <Image src={maletinha} alt='' className="flex w-[1rem] h-[2rem] ml-[1.35rem] pt-[1rem]" />
                                     <h2 className="text-[0.87rem] text-grafite font-semibold py-[0.8rem] ml-[1.35rem]">BeEasy</h2>
@@ -401,7 +386,9 @@ export function Lista(props: ListaProps) {
                                     <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">E-mail:</span> pri@beeasy.com.br </p>
                                     <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">Telefone:</span> (54) 3698-9128</p>
                                 </div>
-                            </SwiperSlide><SwiperSlide>
+                            </SwiperSlide>
+                            
+                            <SwiperSlide>
                                 <div className="pb-[5rem] w-[15.5rem] h-[8.5rem] my-[0.7rem] mx-auto bg-white drop-shadow-xl rounded-lg">
                                     <Image src={maletinha} alt='' className="flex w-[1rem] h-[2rem] ml-[1.35rem] pt-[1rem]" />
                                     <h2 className="text-[0.87rem] text-grafite font-semibold py-[0.8rem] ml-[1.35rem]">Celulah</h2>
@@ -409,7 +396,9 @@ export function Lista(props: ListaProps) {
                                     <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">E-mail:</span> admcelulah@gmail.com </p>
                                     <p className="text-[0.6rem] text-cinza ml-[1.35rem]"> <span className="text-grafite font-medium">Telefone:</span> (54) 9.9697-0208</p>
                                 </div>
-                            </SwiperSlide><SwiperSlide>
+                            </SwiperSlide>
+                            
+                            <SwiperSlide>
                                 <div className="pb-[5rem] w-[15.5rem] h-[8.5rem] my-[0.7rem] mx-auto bg-white drop-shadow-xl rounded-lg">
                                     <Image src={maletinha} alt='' className="flex w-[1rem] h-[2rem] ml-[1.35rem] pt-[1rem]" />
                                     <h2 className="text-[0.87rem] text-grafite font-semibold py-[0.8rem] ml-[1.35rem]">Base Sistemas Ltda</h2>
