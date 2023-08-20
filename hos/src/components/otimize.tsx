19fe6e9af2a9a8bd7b2992a9cd7f008840458bac
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-20 09:23:28 -0300

    Ajustado subheader

--- hos/src/components/otimize.tsx
+++ hos/src/components/otimize.tsx
@@ -8,15 +8,15 @@ interface OtimizeProps {
 export function Otimize(props: OtimizeProps){
     
     return(
-        <div className="bg-fundo -mt-[5.5rem] ">
+        <div className="bg-fundo -mt-[5.5rem] overflow-x-hidden">
             <div className="pt-[6rem] tablet:grid tablet:grid-cols-2 tablet:mx-auto tablet:max-w-[1000px] tablet:pt-[4rem] tablet:justify-center tablet:items-center">
                <div className="mx-auto tablet:col-span-2 tablet:mx-auto tablet:py-[3rem]">
                     <h2 className="text-[1.5rem] pb-[2rem] w-[17rem] mx-auto text-grafite font-extrabold leading-tight text-center tablet:w-[50rem] tablet:flextablet:justify-center tablet:text-center tablet:text-[2rem]">Otimize a <span className="text-vermelho">eficiência</span> dos seus atendimentos</h2>  
                 </div>
             
-                <Image src={pc} alt='' className="mx-auto max-w-[20rem] h-auto flex justify-center items-center self-center -mr-[0.8rem] tablet:col-start-2 tablet:max-w-[31rem] tablet:ml-[7.6rem] tablet:-mb-[3rem]"/>
+                <Image src={pc} alt='' className="mx-auto max-w-[20rem] h-auto flex justify-center items-center self-center -mr-[0.8rem] tablet:col-start-2 tablet:max-w-[35rem] tablet:ml-[4.6rem] tablet:-mb-[5rem]"/>
                 
-                <div className="mx-auto tablet:col-start-1 tablet:-mt-[24rem] tablet:-ml-[3rem]">
+                <div className="mx-auto tablet:col-start-1 tablet:-mt-[24rem] tablet:-ml-[5rem]">
                     <h2 className="text-[1.5rem] pt-[1.8rem] text-grafite font-extrabold leading-tight text-center tablet:text-left tablet:text-[1.75rem] tablet:ml-[8rem] tablet:max-w-[31rem]">Controle de caixa </h2>  
                     <p className="text-[0.87rem] text-grafite mx-auto  w-[17.2rem] text-center tablet:text-left tablet:text-[1.25rem] tablet:ml-[8rem] tablet:w-[30rem]">Tenha segurança nas transações financeiras de maneira prática e eficiente.</p>
 
