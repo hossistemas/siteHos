5a258a4676ceeaa22396fd71ee8d9f8ee129512b
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-21 21:33:20 -0300

    Ajustes gerais landing page

--- hos/src/components/menuitem.tsx
+++ hos/src/components/menuitem.tsx
@@ -10,7 +10,7 @@ interface MenuProps {
 export function Menu({ativo, hrefDesc, descricao} : MenuProps){
     return(
         <Link href={hrefDesc}>
-            <li className={`${ativo ? 'activemenu' : ''} flex w-[11rem]  text-center mx-auto justify-center self-center py-[1.57rem] tablet:py-[2.13rem] tablet:px-[0.3rem] tablet:w-[17rem]`}>{descricao}</li>
+            <li className={`${ativo ? 'activemenu' : ''} flex w-[11rem]  text-center mx-auto justify-center self-center py-[1.57rem] tablet:py-[1.6rem] tablet:px-[0.3rem] tablet:w-[17rem]`}>{descricao}</li>
         </Link>
     )
 }
--- hos/src/components/subheader.tsx
+++ hos/src/components/subheader.tsx
@@ -41,8 +41,8 @@ export function Subheader({primeiro, segundo, terceiro, quarto, hrefPrimeiro, hr
 
     return(
         <>
-         <div className='bg-vermelho w-auto h-[9.4rem] tablet:h-[6.26rem]'>
-            <ul className='grid grid-cols-2  text-[0.87rem]  font-semibold text-fundo tablet:flex tablet:justify-center tablet:mx-auto tablet:gap-x-3 tablet:text-[1rem]  tablet:w-[1200px] tablet:px-[8rem]' >
+         <div className='bg-vermelho w-auto h-[9.4rem] tablet:h-[5rem]'>
+            <ul className='grid grid-cols-2  text-[0.87rem]  font-semibold text-fundo tablet:flex tablet:justify-center tablet:mx-auto  tablet:text-[1rem]  tablet:w-[1200px] tablet:px-[8rem] tablet:h-[5rem]' >
 
                <Menu ativo={menuUm} hrefDesc={`${hrefPrimeiro}`} descricao={`${primeiro}`}  />
 
