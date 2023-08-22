6d749d3fc513ed471910790c013da790cdb42402
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-22 09:02:27 -0300

    Ajustado componente potencialize

--- hos/src/components/potencialize.tsx
+++ hos/src/components/potencialize.tsx
@@ -69,9 +69,9 @@ export function Potencialize(props: PotencializeProps) {
       <div className="carousel overflow-hidden  flex mx-auto h-[19rem] tablet:h-[26rem] tablet:w-[1090px] ">
         <div className="carousel-slides flex transition-transform ease-in-out duration-300  tablet:w-1/3" style={{ transform: `translateX(-${currentIndex *(100 / itemsPerSlide)}%)` }}>
           {images.map((item, index) => (
-            <div key={index} className={`carousel-slide flex-shrink-0 w-[16rem] h-[15rem] mb-[5rem]  mx-[0.8rem] justify-center bg-white  drop-shadow-lg rounded-xl tablet:w-[21.1rem] tablet:h-[22rem] ${index === currentIndex ? 'active' : ''}`}>
-              <Image src={item.image} alt={`Image ${index + 1}`} width={70} height={100} className='flex mx-auto pt-[2rem]  tablet:w-[4rem] tablet:pt-[3rem] tablet:pb-[2rem]'/>
-              <h2 className='text-center text-[1rem] text-grafite font-bold tablet:text-[1.25rem] tablet:px-[1.5rem] tablet:pb-[1.3rem]'>{item.title}</h2>
+            <div key={index} className={`carousel-slide flex-shrink-0 w-[16rem] h-[15rem] mb-[5rem]  mx-[0.8rem] justify-center bg-white  drop-shadow-lg rounded-xl tablet:w-[21.1rem] tablet:h-[22rem] ${index === currentIndex ? 'active' : ''} ${index === 2 ? 'tablet:pt-[1.2rem]' : ''} `} >
+              <Image src={item.image} alt={`Image ${index + 1}`} width={80} height={100} className='flex mx-auto pt-[2rem]  tablet:w-[7rem] tablet:pt-[3rem]'/>
+              <h2 className='text-center text-[1rem] text-grafite font-bold tablet:text-[1.25rem] tablet:px-[1.5rem] tablet:pb-[1.3rem] tablet:pt-[2rem]'>{item.title}</h2>
               <p className='text-center text-[0.8rem] text-grafite tablet:text-[1rem] tablet:w-[18rem] tablet:flex tablet:mx-auto'>{item.text}</p>
             </div>
           ))}
