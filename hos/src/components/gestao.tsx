9d2de80487225f86337ccec1b167e35f6108aad4
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-22 09:18:08 -0300

    Ajustado componente gestao

--- hos/src/components/gestao.tsx
+++ hos/src/components/gestao.tsx
@@ -16,16 +16,16 @@ export function Gestao(props: GestaoProps){
                     <div className="bg-white w-[5.7rem] h-[5.6rem] mt-[1.5rem]  rounded-3xl drop-shadow-2xl flex justify-center mx-auto tablet:rounded-4xl tablet:w-[8.1rem] tablet:h-[8rem]">
                         <Image src={lotes} alt="" className=" w-[3.9rem] h-auto tablet:w-[5rem] tablet:h-auto"/>
                     </div>
-                    <h4 className="w-[13rem] text-[1rem] mt-[1.5rem] font-extrabold text-center text-grafite mx-auto tablet:w-[16.1rem] tablet:text-[1.25rem]">Rastreabilidade de lotes</h4>
-                    <p className="w-[12.25rem] text-[0.87rem] text-center mt-[0.6rem] mx-auto text-grafite tablet:w-[15.75rem] tablet:text-[1rem]">Garanta a qualidade e segurança de produtos, otimizando a cadeia de suprimentos, incluindo gestão de estoques, previsão de demanda e produção eficiente.</p>
+                    <h4 className="w-[13rem] text-[1rem] mt-[1.5rem] font-extrabold text-center text-grafite mx-auto tablet:pt-[2rem] tablet:w-[16.1rem] tablet:text-[1.25rem]">Rastreabilidade de lotes</h4>
+                    <p className="w-[12.25rem] text-[0.87rem] text-center mt-[0.6rem] mx-auto text-grafite tablet:w-[26rem] tablet:text-[1rem]">Garanta a qualidade e segurança de produtos, otimizando a cadeia de suprimentos, incluindo gestão de estoques, previsão de demanda e produção eficiente.</p>
                 </div>
 
                 <div className="">
                     <div className="bg-white w-[5.7rem] h-[5.6rem] mt-[1.5rem] rounded-3xl drop-shadow-2xl flex justify-center mx-auto tablet:rounded-4xl tablet:w-[8.1rem] tablet:h-[8rem]">
                         <Image src={conjugada} alt="" className=" w-[2.9rem] h-auto tablet:w-[4.4rem] tablet:h-auto"/>
                     </div>
-                    <h4 className="w-[13rem] text-[1rem] mt-[1.5rem] font-extrabold text-center text-grafite mx-auto tablet:w-[16.1rem] tablet:text-[1.25rem]">Nota de serviço conjugada</h4>
-                    <p className="w-[12.25rem] text-[0.87rem] text-center mt-[0.6rem] mx-auto text-grafite tablet:w-[15.75rem] tablet:text-[1rem]">Permite a venda de produtos sujeitos ao ICMS e ao ISSQN em um único cupom fiscal, simplificando os processos e garantindo a destinação correta da tributação.</p>
+                    <h4 className="w-[13rem] text-[1rem] mt-[1.5rem] font-extrabold text-center text-grafite mx-auto  tablet:pt-[2rem] tablet:w-[16.1rem] tablet:text-[1.25rem]">Nota de serviço conjugada</h4>
+                    <p className="w-[12.25rem] text-[0.87rem] text-center mt-[0.6rem] mx-auto text-grafite tablet:w-[26rem] tablet:text-[1rem]">Permite a venda de produtos sujeitos ao ICMS e ao ISSQN em um único cupom fiscal, simplificando os processos e garantindo a destinação correta da tributação.</p>
                 </div>
 
             </div>
