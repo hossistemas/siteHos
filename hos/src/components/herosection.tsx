09ee10d044bd7e891c84f7ecf5ec2c6aeab387da
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-22 09:13:46 -0300

    Ajustado componente maximiza

--- hos/src/components/herosection.tsx
+++ hos/src/components/herosection.tsx
@@ -18,13 +18,13 @@ export function HeroSection({title, text, img, bgClassName, titleClassName, text
     return(
         <>
 
-        <div className={`drop-shadow-md mb-[5rem] tablet:pb-[1rem] ${bgClassName}`}>
+        <div className={`drop-shadow-md mb-[5rem] overflow-x-hidden tablet:pb-[1rem] ${bgClassName}`}>
           
             <div className=" mx-auto max-w-[460px] tablet:flex tablet:justify-center tablet:items-center  tablet:max-w-[1200px] tablet:h-[570px]">
 
                 <div className='pt-[3rem] px-[3rem] max-w-[500px] tablet:pl-[2rem] tablet: tablet:max-w-[590px]  '>
                     <h1 className={`text-[1.5rem] font-bold leading-none tablet:text-[2.25rem] ${titleClassName}`}>{title}</h1>
-                    <p className={`text-[0.8rem] font-normal mt-[1rem] mb-[1.5rem] tablet:text-[1.5rem] ${textClassName}`}>{text}</p>
+                    <p className={`text-[0.8rem] font-normal mt-[1rem] mb-[1.5rem] tablet:text-[1.5rem] tablet:pb-[1.5rem] ${textClassName}`}>{text}</p>
                     <Link href="/sobre/contato">
                         <button className={`w-[16rem] text-[1rem] font-regular mb-[1.3rem] px-[1.3rem] py-[0.4rem] rounded-md tablet:text-[1.2rem] tablet:w-[20rem] tablet:h-[3rem] ${buttonClassName}`}>{buttonName}</button>
                     </Link>
