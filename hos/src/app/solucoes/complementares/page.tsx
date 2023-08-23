19f2d5292810a9815d57cb21f53afccedb4dc59b
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-23 18:28:49 -0300

    Alterações em geral

--- hos/src/app/solucoes/complementares/page.tsx
+++ hos/src/app/solucoes/complementares/page.tsx
@@ -23,7 +23,7 @@ export default function Administracao() {
         bgClassName={"bg-fundo"}
         title={"Aprimore sua experiência e torne-a ainda mais completa"}
         titleClassName={"text-grafite"}
-        text={"Centralize e automatize as informações da sua farmácia de maneira fácil, e rápida."}
+        text={"Centralize e automatize as informações da sua farmácia de maneira fácil e rápida."}
         textClassName={"text-grafite"}
         buttonClassName={"bg-gradient-to-b from-laranja to-magenta text-white hover:bg-gradient-to-b hover:from-darker hover:to-darker hover:transition hover:duration-500"}
         img={complementares} buttonName={'Solicite uma demonstração'} imageClassName={''} linkName={"/sobre/contato"}      /> 
--- hos/src/app/suporte/page.tsx
+++ hos/src/app/suporte/page.tsx
@@ -16,7 +16,7 @@ export default function Suporte() {
         title={"Precisa de suporte?"}
         titleClassName={"text-grafite font-bold"}
         text={"Abra um chamado através da nossa Área do Cliente. As respostas ao seu chamado são no período das 8h às 18h (segunda a sexta-feira)."}
-        textClassName={"text-grafite leading-tight w-[35rem]"}
+        textClassName={"text-grafite leading-tight w-[20rem] tablet:w-[35rem]"}
         buttonClassName={"bg-white border-2 border-vermelho text-vermelho  hover:bg-darker hover:border-white hover:text-white hover:transition hover:duration-500"}
         img={suporte} buttonName={'Área do Cliente'} imageClassName={'tablet:h-[500px] tablet:mt-[7rem]'} linkName={"http://cliente.hos.com.br/gerencial/Arearestrita/Autenticacao/Login"}      />
       <Atendimento /> 
