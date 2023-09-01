6d6bf688cfd2fcab437471d63335d15b378c380f
Guilherme da Rocha <guis.darocha@gmail.com>
2023-09-01 07:15:45 -0300

    Alterado area de videos da pagina universidade

--- hos/src/components/videos.tsx
+++ hos/src/components/videos.tsx
@@ -17,45 +17,48 @@ export function Videos(props : VideosProps){
             <p className='text-center text-[1rem] text-grafite w-[15rem] py-[1.5rem] font-bold mx-auto tablet:w-[51rem] tablet:text-[2rem] tablet:pt-[8rem] tablet:pb-[5rem]'>Explore os recursos criados pelos consultores da HOS</p>
 
             <div className='tablet:grid tablet:grid-cols-2 tablet:gap-y-10 tablet:pb-[6rem]'>
-                <div className='mx-auto relative'>
-                    <Link href='https://www.youtube.com/watch?v=_l5KEDfDiDc&ab_channel=HosSistemas' target='_blank'>
-                        <Image src={indicadores} alt='' className='  w-auto h-[12rem] mx-auto tablet:h-[18rem] transition-all ease-in-out duration-300' />
-                        <div className=' absolute bg-cinza top-[4rem] left-[9rem] w-[3.59rem] h-[3.59rem]  rounded-full tablet:top-[7rem] tablet:left-[14rem]  hover:bg-vermelho transition-all ease-in-out duration-500'>
-                            <Image src={play} alt='' className='relative flex justify-center items-center mx-auto mt-4'/>
-                        </div>         
-                    </Link>
-                    <p className='text-center text-[0.87rem] font-semibold mt-2 mx-auto tablet:text-[1.5rem] tablet:w-[24rem] tablet:pt-4'>Indicadores na Gestão de Drogarias</p>
-                </div>
+            <div className='mx-auto relative'>
+                <Link href='https://www.youtube.com/watch?v=_l5KEDfDiDc&ab_channel=HosSistemas' target='_blank'>
+                    <Image src={indicadores} alt='' className='w-auto h-[12rem] mx-auto tablet:h-[18rem] transition-all ease-in-out duration-300' />
+                    <div className='absolute bg-cinza top-[35%] left-[47%] transform translate(-50%, -50%) w-[3.59rem] h-[3.59rem] rounded-full tablet:top-[30%] tablet:left-[40%] tablet:translate(-50%, -50%) hover:bg-vermelho transition-all ease-in-out duration-500'>
+                    <Image src={play} alt='' className='relative flex justify-center items-center mx-auto mt-4' />
+                    </div>
+                </Link>
+                <p className='text-center text-[0.87rem] font-semibold mt-2 mx-auto tablet:text-[1.5rem] tablet:w-[24rem] tablet:pt-4'>Indicadores na Gestão de Drogarias</p>
+            </div>
 
                 <div className='mx-auto py-[2rem] relative w-4/4 tablet:py-0'>
                     <Link href='https://www.youtube.com/watch?v=lm1aYxNGV3E&ab_channel=HosSistemas' target='_blank'>
-                    <Image src={dicas} alt='' className='  w-auto h-[12rem] mx-auto tablet:h-[18rem] transition-all ease-in-out duration-300' />
-                        <div className=' absolute mx-auto bg-cinza top-[6rem] left-[9rem] w-[3.59rem] h-[3.59rem]  rounded-full tablet:top-[7rem] tablet:left-[14rem]  hover:bg-vermelho transition-all ease-in-out duration-500'>
-                            <Image src={play} alt='' className='flex justify-center items-center mx-auto mt-4'/>
-                        </div>  
+                        <Image src={dicas} alt='' className='w-auto h-[12rem] mx-auto tablet:h-[18rem] transition-all ease-in-out duration-300' />
+                        <div className='absolute bg-cinza top-[35%] left-[47%] transform translate(-35%, -50%) w-[3.59rem] h-[3.59rem] rounded-full tablet:top-[30%] tablet:left-[40%] tablet:translate(-50%, -50%) hover:bg-vermelho transition-all ease-in-out duration-500'>
+                        <Image src={play} alt='' className='flex justify-center items-center mx-auto mt-4' />
+                        </div>
                     </Link>
                     <p className='text-center text-[0.87rem] font-semibold mt-2 w-[14rem] mx-auto tablet:text-[1.5rem] tablet:w-[24rem] tablet:pt-4'>Dicas de Gestão Farmacêutica para o Sucesso em Farmácias</p>
                 </div>
 
+
                 <div className='mx-auto relative w-4/4'>
                     <Link href='https://www.youtube.com/watch?v=gyJnJdlnHQI&ab_channel=HosSistemas' target='_blank'>
-                    <Image src={prosperar} alt='' className=' rounded-lg w-auto h-[12rem] mx-auto tablet:h-[18rem] transition-all ease-in-out duration-300' />
-                        <div className=' absolute bg-cinza top-[4rem] left-[9rem] w-[3.59rem] h-[3.59rem]  rounded-full tablet:top-[7rem] tablet:left-[14rem]  hover:bg-vermelho transition-all ease-in-out duration-500'>
-                            <Image src={play} alt='' className='flex justify-center items-center mx-auto mt-4'/>
-                        </div>  
+                        <Image src={prosperar} alt='' className='rounded-lg w-auto h-[12rem] mx-auto tablet:h-[18rem] transition-all ease-in-out duration-300' />
+                        <div className='absolute bg-cinza top-[35%] left-[47%] transform translate(-50%, -50%) w-[3.59rem] h-[3.59rem] rounded-full tablet:top-[30%] tablet:left-[40%] tablet:translate(-50%, -50%) hover:bg-vermelho transition-all ease-in-out duration-500'>
+                        <Image src={play} alt='' className='flex justify-center items-center mx-auto mt-4' />
+                        </div>
                     </Link>
                     <p className='text-center text-[0.87rem] font-semibold mt-2 w-[14rem] mx-auto tablet:text-[1.5rem] tablet:w-[24rem] tablet:pt-4'>Como prosperar no Varejo Farmacêutico</p>
                 </div>
 
+
                 <div className='mx-auto py-[2rem] relative w-4/4 tablet:py-0 tablet:mt-[0.5rem]'>
-                <Link href='https://www.youtube.com/watch?v=RGg3OHoguWo&t=2s' target='_blank'>
-                <Image src={frente} alt='' className=' rounded-lg w-auto h-[12rem] mx-auto tablet:h-[18rem] transition-all ease-in-out duration-300' />
-                        <div className=' absolute bg-cinza top-[4rem] left-[9rem] w-[3.59rem] h-[3.59rem]  rounded-full tablet:top-[7rem] tablet:left-[14rem]  hover:bg-vermelho transition-all ease-in-out duration-500'>
-                            <Image src={play} alt='' className='flex justify-center items-center mx-auto mt-4'/>
-                        </div> 
-                </Link>
+                    <Link href='https://www.youtube.com/watch?v=RGg3OHoguWo&t=2s' target='_blank'>
+                        <Image src={frente} alt='' className='rounded-lg w-auto h-[12rem] mx-auto tablet:h-[18rem] transition-all ease-in-out duration-300' />
+                        <div className='absolute bg-cinza top-[35%] left-[47%] transform translate(-50%, -50%) w-[3.59rem] h-[3.59rem] rounded-full tablet:top-[30%] tablet:left-[40%] tablet:translate(-50%, -50%) hover:bg-vermelho transition-all ease-in-out duration-500'>
+                        <Image src={play} alt='' className='flex justify-center items-center mx-auto mt-4' />
+                        </div>
+                    </Link>
                     <p className='text-center text-[0.87rem] font-semibold w-[14rem] mt-2 mx-auto tablet:text-[1.5rem] tablet:w-[24rem] tablet:pt-2'>HOS Farma: Sistema de Gestão para Farmácias e Drogarias</p>
                 </div>
+
             </div>
         </div>
 
