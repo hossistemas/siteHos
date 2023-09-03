265cf57c72aca21a6f489a6f75ac910edddebaae
Guilherme da Rocha <guis.darocha@gmail.com>
2023-09-03 20:34:23 -0300

    Alterado componente painel

--- hos/src/components/painel.tsx
+++ hos/src/components/painel.tsx
@@ -11,7 +11,7 @@ export function Painel(props: PainelProps){
     
     return(
         <div className="bg-fundo ">
-        <div className=" tablet:mx-auto tablet:grid tablet:grid-cols-2 tablet:pt-[4rem] tablet:w-[1024px]">
+        <div className=" tablet:mx-auto tablet:grid tablet:grid-cols-2 tablet:pt-[4rem] tablet:w-[1400px]">
            <div className="mx-auto tablet:col-start-2 tablet:-mb-[15rem]">
                 <h2 className="text-[1.5rem] text-grafite font-extrabold leading-tight text-center tablet:text-left tablet:text-[2rem]">Painel Dashboard</h2>  
                 <p className="text-[0.87rem] text-grafite mx-auto my-[1.38rem] w-[18.1rem] text-center tablet:text-left tablet:text-[1.25rem] tablet:w-[27rem]">Tenha acesso a dados essenciais de Resumo Geral de Finanças, como vendas versus compras, ticket médio, metas por vendedor, CMV (Custo de Mercadoria Vendida), Contas a Pagar versus Contas a Receber, entre outros.</p>
