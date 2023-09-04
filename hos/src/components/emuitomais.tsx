23853e9e9f107436af10dea50ab28339317b3e85
Guilherme da Rocha <guis.darocha@gmail.com>
2023-09-04 19:20:49 -0300

    Alterado componente emuitomais

--- hos/src/components/emuitomais.tsx
+++ hos/src/components/emuitomais.tsx
@@ -10,17 +10,17 @@ export function Emuitomais(props: EmuitomaisProps){
     return(
         <>
         <div className="px-[2.5rem] py-[2rem] bg-fundo tablet:pt-[10rem] tablet:px-[8rem]">
-            <div className="max-w-[460px] mx-auto tablet:max-w-[1024px] tablet:flex tablet:justify-center tablet:items-center tablet:mx-auto">
+            <div className="max-w-[460px] mx-auto tablet:max-w-[1440px] tablet:flex tablet:justify-center tablet:items-center tablet:mx-auto">
     
                 <div className="pb-[1.7rem] tablet:mr-[4rem]">
-                    <Image src={emuitomais} alt="" className="max-w-[17rem] h-auto mx-auto tablet:w-[33rem] tablet:h-auto"/>
+                    <Image src={emuitomais} alt="" className="max-w-[17rem] h-auto mx-auto tablet:max-w-[35rem] tablet:h-auto"/>
                 </div>
 
                 <div className="max-w-[17rem] mx-auto tablet:max-w-[27rem]">
                     
                     <ul className="list-disc text-[0.8rem]  text-grafite  font-medium ml-[1rem] leading-6 tablet:-mt-[4rem] tablet:leading-loose">
-                        <h2 className="text-[1.5rem] font-bold text-grafite leading-tight mb-[1.5rem] tablet:text-[2rem] ">E muito mais...</h2>
-                        <li className="tablet:text-[1.1rem] font-normal">Tela inicial de alerta da validade dos lotes</li>
+                        <h2 className="text-[1.5rem] font-bold text-grafite leading-tight mb-[1.5rem] tablet:text-[2rem] tablet:ml-[-1.4rem] ">E muito mais...</h2>
+                        <li className="tablet:text-[1.1rem]  font-normal">Tela inicial de alerta da validade dos lotes</li>
                         <li className="tablet:text-[1.1rem] font-normal">Emissão de orçamentos com impressão de documentos relacionados</li>
                         <li className="tablet:text-[1.1rem] font-normal">Formação de preço de medicamentos homeopáticos considerando insumos internos e/ou industrializados</li>
                         <li className="tablet:text-[1.1rem] font-normal">Definição automática da embalagem</li>
